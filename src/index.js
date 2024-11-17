26310f294c5d8715380aad676447d228796a974b
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-17 19:33:49 +0100

    adding atoms molecules organisms and template

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 export { default as App } from './App';
-export * from './context/AuthContent';
+export * from './context/AuthContext';
 export * from './main';
-export * from './styles/breakpoints';
-export * from './styles/themas';
-export * from './styles/variables';
+// export * from './styles/breakpoints';
+// export * from './styles/themas';
+// export * from './styles/variables';
 export * from './supabase/supabase.config';
-export * from './utils/dataEstatica';
+// export * from './utils/dataEstatica';
 
 
