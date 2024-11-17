79763782fb3530ac9ba9647f79975cfcf40ca214
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-17 18:40:31 +0100

    adding Context

---
+++ src/index.js
@@ -0,0 +1,10 @@
+export { default as App } from './App';
+export * from './context/AuthContent';
+export * from './main';
+export * from './styles/breakpoints';
+export * from './styles/themas';
+export * from './styles/variables';
+export * from './supabase/supabase.config';
+export * from './utils/dataEstatica';
+
+
