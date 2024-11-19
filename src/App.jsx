3602ced6f4f552da10f4fb1ad1531a0845a9d249
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-19 21:13:45 +0100

    adding styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import { AuthContextProvider } from "./context/AuthContext"
+import { createContext, useState } from "react";
+import { AuthContextProvider } from "./context/AuthContext";
+import { ThemeProvider } from "styled-components";
 import MyRoutes from "./routers/routes"
+import { Dark, Light } from "./styles/themes";
+
+export const themeContext = createContext(null);
 
 function App() {
+  const [themeuse, setThemeuse] = useState("dark");
+  const theme = themeuse === "light" ? "light" : "dark";
+  const themeStyle = theme === "light" ? Light : Dark;
+
 
   return (
     <>
-      <AuthContextProvider>
-        <div className="grid grid-cols-1">
-          <MyRoutes />
-        </div>
-      </AuthContextProvider>
+      <themeContext.Provider value={{ theme, setThemeuse }}>
+        <ThemeProvider theme={themeStyle}>
+          <AuthContextProvider>
+            <div className="grid grid-cols-1">
+              <MyRoutes />
+            </div>
+          </AuthContextProvider>
+        </ThemeProvider>
+      </themeContext.Provider>
     </>
   )
 }
