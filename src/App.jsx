6db2a6c6714d72800a68ac71f59131b862851fbf
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-19 22:41:11 +0100

    adding sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,29 @@ export const themeContext = createContext(null);
 
 function App() {
   const [themeuse, setThemeuse] = useState("dark");
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   const theme = themeuse === "light" ? "light" : "dark";
   const themeStyle = theme === "light" ? Light : Dark;
-
 
   return (
     <>
       <themeContext.Provider value={{ theme, setThemeuse }}>
         <ThemeProvider theme={themeStyle}>
           <AuthContextProvider>
-            <div className="grid grid-cols-1">
-              <MyRoutes />
+            <div className={`${sidebarOpen ? "active" : ""}bg-[#1C1C1E] text-white grid grid-cols-1 md:grid-cols-2`}>
+
+              <section className="Sidebar hidden md:block">
+                Sidebar
+              </section>
+              <section className="Menu block absolute ml-5 md:hidden">
+                Menu hamburguesa
+              </section>
+              <section className="routes grid grid-cols-1 w-full">
+                <MyRoutes />
+              </section>
+
+
             </div>
           </AuthContextProvider>
         </ThemeProvider>
