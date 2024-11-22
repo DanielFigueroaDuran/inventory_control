39904c6fac1622bec5c8032c17b741497869d8ea
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-22 22:16:59 +0100

    addind AuthStorage

--- src/store/AuthStore.jsx
+++ src/store/AuthStore.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import { create } from "zustand";
-export const useAuthStore=create((set,get)=>({
-    signInWithEmail: async (p)=>{
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email: p.correo,
-            password: p.pass
-          })
-          if(error){
-            return null;
-          }
+import { supabase } from "../supabase/supabase.config";
+
+export const useAuthStore = create((set, get) => ({
+  signInWithEmail: async (params) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: params.correo,
+      password: params.pass
+    })
+    if (error) {
+      return null;
     }
+  },
+  signOut: async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      throw new error(`A ocurrido un error durante el cierre ${error}`);
+    }
+  }
 }))
